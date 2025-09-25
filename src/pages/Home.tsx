0839411aa049f93b036b013040f6ba16ded880ca
PedroClaes <pedrogabrielclaes@gmail.com>
2025-09-25 14:03:47 -0300

    Criação da página home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,22 @@
+import { useState } from "react";
+import type { StudySession } from "../types/StudySession";
+import { SessionCard } from "../components/Card";
+
+
+const initialSessions: StudySession[] = [
+  { id: "1", subject: "React", minutes: 90, date: "2025-09-21" },
+  { id: "2", subject: "Estruturas de Dados", minutes: 120, date: "2025-09-22" }
+];
+
+export function Home() {
+  const [sessions] = useState(initialSessions);
+  return (
+    <div>
+      <h1 className="text-2xl mb-4">Minhas Sessões de Estudo</h1>
+      <div className="grid gap-2">
+        {sessions.map(s => <SessionCard key={s.id} session={s} />)}
+      </div>
+    </div>
+  );
+}
 
--- src/pages/NewSection.tsx
+++ src/pages/NewSection.tsx
@@ -1,11 +1,2 @@
-import type { StudySession } from "../types/StudySession";
 
-type Props = { session: StudySession };
-export function SessionCard({ session }: Props) {
-  return (
-    <div className="border rounded p-4 shadow-md">
-      <h2 className="font-bold">{session.subject}</h2>
-      <p>{session.minutes} min em {session.date}</p>
-    </div>
-  );
-}
+
